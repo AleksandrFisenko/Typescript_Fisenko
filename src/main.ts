ea1c425d7c545aca70f2250179d6e9913d0f6534
Aleksandr Fisenko <a.fisenko@tg.dunice.net>
2024-07-11 16:36:55 +0300

    task 2

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,55 @@
-// Возможные роли пользователя - user/moderator/admin
-// Tags: Enum, Union, Type, Interface
+// Виды пасты - spagetti/penne/macaroni
+// Tags: Union, Enum, Narrowing
 
-enum Roles {
-  user = "user",
-  moderator = "moderator",
-  admin = "admin"
+enum PastaType {
+  spagetti = "spagetti",
+  penne = "penne",
+  macaroni = "macaroni",
 }
 
-interface User {
-  id: number;
-  login: string;
-  role: Roles;
-}
-
-const user: User = {
-  id: 1,
-  login: 'test',
-  role: Roles.user,
-}
-
-type CheckPermissionsFunction = {
-  (user: User): boolean
-}
-
-const checkPermissionsDecorator = (roles: Roles[]): CheckPermissionsFunction => {
-  return (user: User) => {
-    return roles.includes(user.role);
+class Pizza {
+  hasPepperoni: boolean;
+  hasSauce: boolean;
+  hasCheese: boolean;
+  bakeTime = 30;
+  constructor(hasPepperoni: boolean, hasSauce: boolean, hasCheese: boolean) {
+    this.hasPepperoni = hasPepperoni;
+    this.hasSauce = hasSauce;
+    this.hasCheese = hasCheese;
+  }
+  bake() {
+    setTimeout(console.log, this.bakeTime, 'Enjoy your pizza!');
   }
 }
-const checkPermission = checkPermissionsDecorator([Roles.admin]);
-const hasPermissions = checkPermission(user);
-console.log(hasPermissions);
+class Pasta {
+  pastaType;
+  hasSauce;
+  hasCheese;
+  cookTime = 30;
+  constructor(pastaType: PastaType, hasSauce: boolean, hasCheese: boolean) {
+    this.pastaType = pastaType;
+    this.hasCheese = hasCheese;
+    this.hasSauce = hasSauce;
+  }
+  cook() {
+    setTimeout(console.log, this.cookTime, 'Enjoy your pasta!');
+  }
+}
+class Kitchen {
+  makeDish(dish: Pizza | Pasta) {
+    // эту часть надо переделать
+    if (dish instanceof Pizza) {
+      dish.bake();
+      return dish;
+    } else if (dish instanceof Pasta) {
+      dish.cook();
+      return dish;
+    }
+    throw new Error('Unknown dish');
+  }
+}
+const kitchen = new Kitchen();
+const pizza = new Pizza(true, true, false);
+const pasta = new Pasta(PastaType.spagetti, true, true);
+kitchen.makeDish(pizza);
+kitchen.makeDish(pasta);
