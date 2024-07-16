1255b14e079e18ef8978acac99b18e66c3a04efa
Aleksandr Fisenko <a.fisenko@tg.dunice.net>
2024-07-16 10:17:04 +0300

    task 4-5

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 
-// spread
-interface x {
-  [index: number]: number | boolean,
-}
 
 
 
-const Arr: x = [false, 5, 5, 5, true]
-const Arr2: x = [false, 5, true]
-const Arr3: x = [true, 1, 2, 3, 4, 5, true]
+
